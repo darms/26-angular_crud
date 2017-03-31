5227327a241159d812009af1fd8a460b6d9cbbe2
darms <darmcca@gmail.com>
2017-03-30 18:55:58 -0700

    gallery and navbar components have been created

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const production = process.env.NODE_ENV === 'production';
+
+dotenv.load();
+
 module.exports = {
+  devtool: 'eval',
   entry: `${__dirname}/app/entry.js`,
   output: {
     filename: 'bundle.js',
@@ -13,7 +20,11 @@ module.exports = {
     new HTMLPlugin({
       template: `${__dirname}/app/index.html`
     }),
-    new ExtractTextPlugin('bundle.css')
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.DefinePlugin({
+      __API_URL__: JSON.stringify(process.env.API_URL),
+      __DEBUG__: JSON.stringify(!production)
+    })
   ],
   module: {
     rules: [
