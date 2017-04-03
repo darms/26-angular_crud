5a0b7f05a084fff93436709abc8588bf8b74fbcb
darms <darmcca@gmail.com>
2017-04-03 08:43:29 -0700

    edited gallery item

---
+++ app/component/gallery/gallery-item/gallery-item.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('./_gallery-item.scss');
+
+module.exports = {
+  template: require('./gallery-item.html'),
+  controller: ['$log', 'galleryService', GalleryItemController],
+  controllerAs: 'galleryItemCtrl',
+  bindings: {
+    gallery: '<'
+  }
+};
+
+function GalleryItemController($log, galleryService) {
+  $log.debug('GalleryIemController');
+
+  this.showEditGallery = false;
+
+  this.deleteGallery = function() {
+    galleryService.deleteGallery(this.gallery._id)
+
+  };
+};
